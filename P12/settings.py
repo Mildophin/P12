ddb641d11c1b50dab720c5e1b51ed9fd5516f585
Milan Viricel <viricel.milan@gmail.com>
2022-03-29 10:06:41 +0200

    Maj postgresql

--- P12/settings.py
+++ P12/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'P12.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'P12',
+        'USER': 'postgres',
+        'PASSWORD': 'M850058v!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
